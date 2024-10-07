23461e982441fb48991595b24975f0596d5a13c3
dhanuprys <dhanuprys@gmail.com>
2024-10-07 22:32:03 +0800

    Test

--- modules/web.sb/var/www/html/src/data/repositories/PlaceRepository.ts
+++
@@ -1,9 +0,0 @@
-import PlaceEntity from '../entities/PlaceEntity';
-
-interface PlaceRepository {
-    getPlaceById(id: string): Promise<PlaceEntity | null>;
-    getPlacesByCategory(category: string): Promise<PlaceEntity[]>;
-    getAllPlaces(): Promise<PlaceEntity[]>;
-}
-
-export default PlaceRepository;
