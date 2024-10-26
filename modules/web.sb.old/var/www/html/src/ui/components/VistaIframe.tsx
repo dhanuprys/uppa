0d3e7a974d25b9dc36fa05fa67d71da4acf268c0
dhanuprys <dhanuprys@gmail.com>
2024-10-26 14:48:29 +0800

    Remove unused package

--- modules/web.sb.old/var/www/html/src/ui/components/VistaIframe.tsx
+++
@@ -1,9 +0,0 @@
-function VistaIframe({ className, index }: { className?: string, index: number }) {
-    return (
-        <div className={className ?? ''}>
-            <iframe className="w-full h-screen" title="WorldView" src={`/3dvista/index.htm?media-index=${index}`} loading="eager"></iframe>
-        </div>
-    );
-}
-
-export default VistaIframe;
