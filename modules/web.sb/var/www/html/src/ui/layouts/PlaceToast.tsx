b89da71ef2100c8fe472937c684e4a63fc2a79ae
dhanuprys <dhanuprys@gmail.com>
2024-10-26 20:04:03 +0800

    Fix width and height

--- modules/web.sb/var/www/html/src/ui/layouts/PlaceToast.tsx
+++ modules/web.sb/var/www/html/src/ui/layouts/PlaceToast.tsx
@@ -28,18 +28,18 @@ function PlaceToast({
     }
 
     return (
-        <div className={`${className || ''} transition-all md:duration-300 ${opened ? 'duration-0 md:duration-500 delay-100 !w-screen !h-screen top-0 z-20' : ''} -translate-y-full md:translate-y-0 md:translate-x-full ${activePlace ? '!translate-y-0 md:!translate-x-0' : ''} fixed top-0 right-0 w-screen min-w-screen min-h-[150px] md:w-[450px] md:min-w-[500px] lg:min-w-[560px]`}>
+        <div className={`${className || ''} max-h-screen overflow-auto transition-all md:duration-300 ${opened ? 'duration-0 md:duration-500 delay-100 !w-screen !h-screen top-0 z-20' : ''} -translate-y-full md:translate-y-0 md:translate-x-full ${activePlace ? '!translate-y-0 md:!translate-x-0' : ''} fixed top-0 right-0 w-screen min-w-screen min-h-[150px] md:w-[450px] md:min-w-[500px] lg:min-w-[560px]`}>
             <div className={`transition-all md:duration-500 h-full w-full p-4 md:p-8 ${opened ? 'duration-0 md:delay-100 !p-0 md:!p-0' : ''}`}>
                 <div className={`h-full relative grid grid-cols-3 gap-4 md:items-center bg-white border shadow-sm md:shadow-xl rounded-3xl px-4 py-4 md:py-2 md:!p-4 ${opened ? 'delay-[850ms] !p-0 !rounded-none before:!hidden' : ''} before:z-[-1] before:bg-gradient-to-r before:from-blue-600 before:hidden md:before:block before:absolute before:top-0 before:left-0 before:size-full before:rounded-3xl before:-translate-x-2 before:-translate-y-2`}>
 
                     {/** DETAIL  */}
-                    {!is360View && <div className={`flex gap-2 order-1 md:order-none ${opened ? 'col-span-3 md:col-span-1 flex-col items-center p-8' : 'col-span-3'}`}>
+                    {!is360View && <div className={`flex gap-2 order-1 bg-white md:order-none ${opened ? 'col-span-3 md:col-span-1 flex-col items-center p-8' : 'col-span-3'}`}>
                         <div className={`shrink-0 ${opened ? '' : 'hidden md:block'}`}>
                             <img alt="route-vector" className={`${opened ? 'w-[200px]' : 'w-[90px]'}`} src={RouteImage} />
                         </div>
                         <div className={`${opened ? 'text-center space-y-6' : ''}`}>
                             <h2 className={`text-lg md:text-xl ${opened ? 'font-semibold' : ''}`}>{activePlace?.name || 'Kampus Jineng Dalem'}</h2>
-                            <p className={`text-slate-500 leading-4 md:leading-normal ${opened ? 'text-sm md:text-lg' : 'text-xs md:text-sm'}`}>{activePlace?.description.slice(0, 100) + '...' || 'Dolor velit dolore fugiat reprehenderit eu non. Sint ut est incididunt occaecat incididunt qui'}</p>
+                            <p className={`text-slate-500 leading-4 md:leading-normal ${opened ? 'text-sm md:text-lg' : 'text-xs md:text-sm'}`}>{activePlace?.description[opened ? 'toString' : 'slice'](0, 100) + (opened ? '' : '...') || 'Dolor velit dolore fugiat reprehenderit eu non. Sint ut est incididunt occaecat incididunt qui'}</p>
                             <div className={`mt-2 flex gap-x-2 ${opened ? 'justify-center' : 'justify-end md:justify-start'}`}>
                                 {
                                     opened
@@ -55,7 +55,7 @@ function PlaceToast({
                     </div>}
 
                     {/** PREVIEW 3D VISTA */}
-                    {opened && <div className={`${is360View ? 'absolute w-screen' : 'relative'} col-span-3 md:col-span-2 bg-white h-full`}>
+                    {opened && <div className={`${is360View ? 'fixed top-0 left-0 w-screen h-screen' : 'relative'} col-span-3 md:col-span-2 bg-white min-h-[50vh] md:min-h-auto h-full`}>
                         <VistaIframe iframeClassName={is360View ? '' : 'md:rounded-2xl'} className={`size-full ${is360View ? '' : 'rounded-2xl'}`} index={activePlace?.vistaIndex} />
                         {
                             is360View
